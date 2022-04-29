ad974abb595bf9df36e0f1923613ccbf015c369b
matthiasgrafe <matthias-grafe@web.de>
2022-04-29 12:21:17 +0200

    Version 2

--- programm.py
+++ programm.py
@@ -1,10 +1,18 @@
-from machine import Pin, ADC
-from time import sleep
 
-sharp = ADC(Pin(34))
-sharp.atten(ADC.ATTN_11DB)       #Full range: 3.3v
+# Sharp GP2Y0A21 Infrarot Entfernungsmesser
+# Matthias Grafe
+# 29.04.22
+# Quelle: https://microcontrollerslab.com/esp32-esp8266-adc-micropython-measure-analog-readings/
+
+from machine import Pin, ADC       #Klasse PIN und ADC importieren
+from time import sleep             #Klasse sleep importieren
+
+infrarot = ADC(Pin(37))            #Objekt Infrarot
+infrarot.atten(ADC.ATTN_6DB)      #3.3V Spannungsbereich
+infrarot.width(ADC.WIDTH_10BIT)   # Wert werden nur im Bereich zwischen 0-1023 ausgegeben
+
 
 while True:
-  sharp_value = sharp.read()
-  print(sharp_value)
-  sleep(0.1)
+ infrarot_value = infrarot.read()   # Analog Pin auslesen
+ print(infrarot_value)              # Wert vom Sensor ausgeben
+ sleep(0.25)
